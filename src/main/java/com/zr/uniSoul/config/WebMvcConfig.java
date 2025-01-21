b16b65cb7e3af87900dfc0e232dde60eeb03ed8a
fangcun <2952262748@qq.com>
2025-01-22 01:50:06 +0800

    项目模板更新

--- src/main/java/com/zr/uniSoul/config/WebMvcConfig.java
+++ src/main/java/com/zr/uniSoul/config/WebMvcConfig.java
@@ -16,5 +16,6 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开始进行静态资源映射...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+        registry.addResourceHandler("/UniSoul/**").addResourceLocations("classpath:/UniSoul/");
     }
 }
---
+++ src/main/java/com/zr/uniSoul/pojo/dto/userDTO.java
@@ -0,0 +1,13 @@
+package com.zr.uniSoul.pojo.dto;
+/**
+ * @Description: 用户数据传输对象
+ */
+public class userDTO {
+    private String username;//用户名
+    private String password;//密码
+    private String email;//邮箱
+    private String phone;//电话
+    private String sex;//性别
+    private String birthday;//生日
+
+}
