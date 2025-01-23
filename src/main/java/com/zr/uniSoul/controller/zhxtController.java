f714c623f3d1efbe3483496d20053e32b5145ed1
19150118127 <1307946378@qq.com>
2025-01-24 00:09:30 +0800

    项目骨架拓展

---
+++ src/main/java/com/zr/uniSoul/controller/zhxtController.java
@@ -0,0 +1,17 @@
+package com.zr.uniSoul.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 智绘心途板块代码
+ */
+@RestController
+@RequestMapping("/zhxt")
+@Api(tags = "智绘心途")
+@Slf4j
+public class zhxtController {
+
+}
---
+++ src/main/java/com/zr/uniSoul/mapper/zxwlMapper.java
@@ -0,0 +1,7 @@
+package com.zr.uniSoul.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface zxwlMapper {
+}
